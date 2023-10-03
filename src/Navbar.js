8202b6b667e28d460c4e62404017cebdd5db3406
Errol Fernandes <83249470+ErrolFernandes@users.noreply.github.com>
2023-10-03 15:14:49 +0530

    Update

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,13 @@
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>The Dojo Blog</h1>
+      <h1>22</h1>
+      <h5>A collection of indie built tools</h5>
+      <p> We’ve built a collection of well designed functional tools for developers and indie-hackers to make their life x100 times easier. </p>
       <div className="links">
-        <a href="/">Home</a>
-        <a href="/create">New Blog</a>
+      <a href="">Abhishek K</a>
+      <a href="">Jason D Souza</a>
+      <a href="">Errol Fernandes</a>
       </div>
     </nav>
   );
