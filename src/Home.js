77f1131cb8812eae37b379c337ae04e1c8594cdd
Errol Fernandes <83249470+ErrolFernandes@users.noreply.github.com>
2023-10-03 16:10:15 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,22 @@
+import { useState } from "react";
+
 const Home = () => {
+  const [products, setProducts] = useState([
+    { title: 'Datamation', address: '', id: 1 },
+    { title: 'Retro', address: '', id: 2 },
+    { title: 'Eight', address: '', id: 3 }
+  ])
+
   return (
     <div className="home">
-      <h2></h2>
+      {products.map(product => (
+        <div className="products-preview" key={product.id} >
+          <p>{ pruduct.id }</p>
+          //gif address
+          <h5>{ product.title }</h5>
+          
+        </div>
+      ))}
     </div>
   );
 }
