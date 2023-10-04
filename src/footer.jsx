3e94a941b06e1f7f410c926c851f68468ab0b2ed
Errol Fernandes <83249470+ErrolFernandes@users.noreply.github.com>
2023-10-04 18:25:34 +0530

    Add files via upload

--- src/footer.jsx
+++ src/footer.jsx
@@ -1,6 +1,13 @@
 const Footer = () =>{
     return (
-        <h5>Footer</h5>
+        <div>
+            <div className="socials">
+            <img src="/Assets/Rectangle 26.png"></img>
+            <img src="/Assets/Ellipse 5.png"></img>
+            <img src="/Assets/Polygon 1.png"></img>
+            </div>
+                        <h5>&copy;22Group</h5>
+        </div>
     )
 }
 
